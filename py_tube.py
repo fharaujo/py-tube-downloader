8c80e20b28e020ead27b0571b98ad3cb6d92b382
fharaujo <araujofabio2012@gmail.com>
2021-04-29 19:33:19 -0300

    app version 1.0 and partlearning python

---
+++ py_tube.py
@@ -0,0 +1,49 @@
+from pytube import YouTube, Playlist
+
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
+print('\n ** Py-Tube-Youtube Download ***')
+print('Todo vídeo é baixado na qualidade 720p (Se estiver disponível)\n')
+print('Opções do programa: ')
+
+
+def init():
+    print('*-> [1] Um para escolher DOWNLOAD \n*-> [0] Zero para sair\n')
+    try:
+        program = int(input('Digite a opção: '))
+        if program > 0 and program == 1:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            program_main()
+        elif program < 0 or program > 1:
+            print('# Opção inválida # ')
+            init()
+    except ValueError:
+        print('\n## Digite um opção válida ##\n')
+        init()
+
+
+def program_main():
+    option = input('Digite (v) para um VÍDEO ou (p) para PLAYLIST: ').lower()
+    if option == 'v':
+        link = input('\nCole o link do vídeo aqui: ')
+        video = YouTube(link)
+        stream = video.streams.filter(progressive=True)
+        down_video = video.streams.get_by_itag(22)
+        print(f'Download começou... {video.title}')
+        down_video.download()
+        print('Fim do Download!!!')
+    elif option == 'p':
+        link = input('\nCole o link da playlist aqui: ')
+        playlist = YouTube(link)
+        for video in playlist.videos:
+            print(f'Download começou... {video.title}')
+            video.streams.first().download()
+        print('Fim do Download!!!')
+    else:
+        print('Digite uma oção válida.')
+        program_main()
+
+
+init()
+print('\n\nPy-tube-Download - 1.0 - @faraujo')
